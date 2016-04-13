da7b74bf6100f7b0203ae5dd1dbb7501254732b4
Kate Riesbeck <kriesbeck@gmail.com>
2016-04-12 20:37:34 -0400

    customizing html templates

--- ddl_flask_heroku/app/views.py
+++ ddl_flask_heroku/app/views.py
@@ -5,18 +5,6 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'nickname': 'Miguel'} # fake user
-	posts = [ #fake array of posts
-		{
-			'author':{'nickname':'John'},
-			'body': 'Beautiful day in Portland!'
-		}, 
-		{
-			'author':{'nickname':'Susan'},
-			'body': 'The Avengers Movie was so cool!'
-		}
-	]
 	return render_template('index.html',
-							title='Home',
-							user = user,
-							posts = posts)
+							title='Home'
+							)
