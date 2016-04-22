53d5f2f041feb8946cab8c2efbfee010531e0553
Judy Yang <judy.s.yang@gmail.com>
2016-04-21 20:17:24 -0400

    version april 21 2016

--- NPR_flask/app/views.py
+++ NPR_flask/app/views.py
@@ -8,3 +8,17 @@ def index():
 	return render_template('index.html',
 							title='Home'
 							)
+
+
+@app.route('/about')
+def about():
+	return render_template('about.html',
+							title='About'
+							)
+
+@app.route('/vizualizations')
+def vizualizations():
+	return render_template('vizualizations.html',
+							title='Vizualizations'
+							)
+
